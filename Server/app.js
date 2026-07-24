4f62a4c874c1ce4fb2f654294e741c4a9b669b53
MaitriJoshi1805 <maitrijoshi.official@gmail.com>
2026-07-24 12:02:11 +0530

    Completed backend vehicle tracking with Redis and Socket.IO

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const vehicleRoutes = require("./routes/vehicleRoutes");
+
 const app = express();
 
 app.use(cors());
@@ -9,5 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("FleetDash Backend Running");
 });
+
+// Vehicle API
+app.use("/api/vehicles", vehicleRoutes);
 
 module.exports = app;
